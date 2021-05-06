c5c54426c22cb0be5e6fb1c387f5517687640d73
cristhofert <cristhofert97@gmail.com>
2021-05-06 20:16:09 +0000

    add & update

--- src/js/component/todo.js
+++ src/js/component/todo.js
@@ -1,18 +1,42 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 const ToDo = () => {
 	const [newTask, setNewTask] = useState("");
-	const [toDoList, setToDoList] = useState(["test1", "test2", "test3"]);
-	const refRemove = useRef(null);
+	const [toDoList, setToDoList] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
 
 	const add = e => {
 		e.preventDefault();
-		setToDoList(prev => [...prev, newTask]);
+		setToDoList(prev => [...prev, { label: newTask, done: false }]);
 		setNewTask("");
+		updateData(toDoList);
 	};
 
 	const remove = index => e => {
 		setToDoList(prev => prev.filter((item, i) => i != index));
+	};
+
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/cristhofert")
+			.then(res => res.json())
+			.then(data => setToDoList(data))
+			.catch(error => console.log(error, "ERROR"));
+	};
+
+	const updateData = items => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/cristhofert", {
+			method: "PUT",
+			body: JSON.stringify(items),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(data => console.log(data.result))
+			.catch(err => console.log(err, "PUT ERROR!"));
 	};
 
 	return (
@@ -45,16 +69,20 @@ const ToDo = () => {
 								</div>
 							</form>
 						</li>
-						{toDoList.map((item, index) => (
-							<li className="list-group-item" key={index}>
-								<div className="d-flex justify-content-between p-0">
-									<p className="m-0">{item}</p>
-									<i
-										className="fas fa-times text-muted"
-										onClick={remove(index)}></i>
-								</div>
-							</li>
-						))}
+						{toDoList.length === 0 ? (
+							<li className="list-group-item">Cargando...</li>
+						) : (
+							toDoList.map((item, index) => (
+								<li className="list-group-item" key={index}>
+									<div className="d-flex justify-content-between p-0">
+										<p className="m-0">{item.label}</p>
+										<i
+											className="fas fa-times text-muted"
+											onClick={remove(index)}></i>
+									</div>
+								</li>
+							))
+						)}
 					</ul>
 				</div>
 			</div>
